56469e4eb2208d3dd96d6c953cace703f93db846
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2024-05-13 11:18:37 +0700

    masukkan variable nim dan nama

--- StatikFunction2/StatikFunction2.cpp
+++ StatikFunction2/StatikFunction2.cpp
@@ -5,16 +5,16 @@ using namespace std;
 class mahasiswa
 {
 private:
-	static int nim;
+	static long int nim;
 public:
-	int id;
+	long int id;
 	string nama;
 
 	void setID();
 	void printAll();
 
-	static void setNim(int pNim) { nim = pNim; }
-	static int getNim() { return nim; }
+	static void setNim(long int pNim) { nim = pNim; }
+	static long int getNim() { return nim; }
 
 	mahasiswa(string pnama)
 	{
@@ -31,3 +31,28 @@ void mahasiswa::setID()
 }
 
 
+void mahasiswa::printAll()
+{
+	cout << "ID   = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
+
+
+int main()
+{
+	mahasiswa mhs1("Rafli Ramadhani");
+	mahasiswa mhs2("Azkal Azkia");
+	mahasiswa::setNim(9);
+	mahasiswa mhs3("Rafi Ramadhan");
+	mahasiswa mhs4("Damar Sadewa");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+
+	cout << "akses dari luar object = " << mahasiswa::getNim() << endl;
+
+	return 0;
+}
---
+++ constructor/constructor.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa
+{
+public:
+	int nim;
+	string nama;
+};
